8334c9d8bfa4acacf485b43206334869701e2c5e
Viktor <viktor4strlsov@gmail.com>
2024-05-17 19:12:47 +0600

    main

--- src/components/theFooter/TheFooter.tsx
+++ src/components/theFooter/TheFooter.tsx
@@ -1,52 +1,39 @@
 import styles from './TheFooter.module.scss'
-import CarIcon from '@/components/theFooter/icons/CarIcon'
-import ClassssIcon from '@/components/theFooter/icons/ClassssIcon'
-import ProsentIcon from '@/components/theFooter/icons/ProsentIcon'
-import FileBlockIcon from '@/components/theFooter/icons/FileBlockIcon'
-import Logo from '@/components/icons/Logo'
-import Facebook from '@/components/theFooter/icons/facebook'
-import Twiter from '@/components/theFooter/icons/twiter'
-import Insta from '@/components/theFooter/icons/insta'
-import Youtybe from '@/components/theFooter/icons/youtybe'
-import Acounti from '@/components/theFooter/icons/acounti'
+import Phone from "@/components/theFooter/icons/phone";
+import Location from "@/components/theFooter/icons/location";
+import Message from "@/components/theFooter/icons/message";
+import Facebook from "@/components/theFooter/icons/facebook";
+import Instagram from "@/components/theFooter/icons/instagram";
+import Twiter from "@/components/theFooter/icons/twiter";
+
+
 
 const TheFooter = () => {
 	return (
 		<footer className={styles.footer}>
-			<div className={styles.contentYslug}>
-				<div className={styles.yslygi}>
-					<div className={styles.blockYsl}>
-						<div><CarIcon /></div>
-						<div className={styles.text}>Бесплатная доставка</div>
-					</div>
-					<div className={styles.blockYsl}>
-						<div><ClassssIcon /></div>
-						<div className={styles.text}>Экспертное обслуживание клиентов</div>
-					</div>
-					<div className={styles.blockYsl}>
-						<div><ProsentIcon /></div>
-						<div className={styles.text}>Удивительная ценность</div>
-					</div>
-					<div className={styles.blockYsl}>
-						<div><FileBlockIcon /></div>
-						<div className={styles.text}>Безопасная оплата</div>
-					</div>
-				</div>
+			<div className={styles.blockInfo}>
+				<ul className={styles.blockText}>
+					<li className={styles.nameHead}>Документы</li>
+					<li className={styles.text}>Политика конфиденциальности</li>
+					<li className={styles.text}>Пользовательское соглашение</li>
+					<li className={styles.text}>Лицензионное соглашение</li>
+					<li className={styles.text}>Оферта</li>
+				</ul>
+				<ul className={styles.blockText}>
+					<li className={styles.nameHead}>Контакты</li>
+					<li className={styles.text}><Phone/>  +7 (495) 260-17-78</li>
+					<li className={styles.text}><Location/> Москва, Ленинградский проспект, д. 37, корп. 3</li>
+					<li className={styles.text}><Message/> info@gruzi.ru</li>
+				</ul>
 			</div>
-			<div className={styles.blockAcounti}>
-				<div className={styles.follow}>
-					<div className={styles.followText}>Follow Us</div>
-					<ul className={styles.siti}>
-						<li><Facebook /></li>
-						<li><Twiter /></li>
-						<li><Insta /></li>
-						<li><Youtybe /></li>
+			<div className={styles.infoEnd}>
+				<div className={styles.blockEnd}>
+					<p className={styles.textEnd}>Все права защищены и принадлежат торговой марке Грузи.ру </p>
+					<ul className={styles.icons}>
+						<li><Facebook/></li>
+						<li><Instagram/></li>
+						<li><Twiter/></li>
 					</ul>
-				</div>
-				<div className={styles.logo}><Logo /></div>
-				<div className={styles.follow}>
-					<div className={styles.followText}>We Accept</div>
-					<div><Acounti /></div>
 				</div>
 			</div>
 		</footer>
